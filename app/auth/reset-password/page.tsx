c9bde9a8ef9f49db5dec9c15c27507b820378902
parvesmosarof35 <parvesmosarof35@gmail.com>
2026-01-19 03:58:23 +0600

    first commit

---
+++ app/auth/reset-password/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+
+import Image from "next/image";
+
+import { useRouter } from "next/navigation";
+
+function ResetPassword() {
+  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+
+  return (
+    <div className="bg-white min-h-screen flex items-center justify-center p-5">
+      <div className="container mx-auto">
+        <div className="flex  justify-center items-center">
+          <div className="w-full lg:w-1/2 bg-white p-5 md:px-18 md:py-28 shadow-[0px_10px_20px_rgba(0,0,0,0.2)] rounded-2xl">
+            <div className="flex justify-center items-center">
+               <Image className="mx-auto my-5" src="/clientLogo.png" alt="Logo" width={200} height={200} />
+            </div>
+            <h2 className="text-[#0D0D0D] text-2xl  font-bold text-center mb-5">
+              Set a new password
+            </h2>
+            <p className="text-[#6A6D76] text-center mb-10">
+              Create a new password. Ensure it differs from previous ones for
+              security
+            </p>
+            <form className="space-y-6">
+              <div className="w-full">
+                <label className="text-xl text-[#0D0D0D] mb-2 font-bold">
+                  New Password
+                </label>
+                <div className="w-full relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    placeholder="**********"
+                    className="w-full px-5 py-3 border-2 border-[#6A6D76] rounded-md outline-none mt-5 placeholder:text-xl"
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 bottom-4 flex items-center text-[#6A6D76]"
+                  >
+                    {showPassword ? (
+                      <EyeOff className="w-5 h-5" />
+                    ) : (
+                      <Eye className="w-5 h-5" />
+                    )}
+                  </button>
+                </div>
+              </div>
+              <div className="w-full">
+                <label className="text-xl text-[#0D0D0D] mb-2 font-bold">
+                  Confirm New Password
+                </label>
+                <div className="w-full relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    placeholder="**********"
+                    className="w-full px-5 py-3 border-2 border-[#6A6D76] rounded-md outline-none mt-5 placeholder:text-xl"
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 bottom-4 flex items-center text-[#6A6D76]"
+                  >
+                    {showPassword ? (
+                      <EyeOff className="w-5 h-5" />
+                    ) : (
+                      <Eye className="w-5 h-5" />
+                    )}
+                  </button>
+                </div>
+              </div>
+              <div className="flex justify-center items-center">
+                <button
+                  onClick={() => router.push("/auth")}
+                  type="button"
+                  className="w-1/3 bg-[#00c0b5] text-white font-bold py-3 rounded-lg shadow-lg cursor-pointer mt-5"
+                >
+                  Update Password
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ResetPassword;
